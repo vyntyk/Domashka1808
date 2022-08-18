f349e1b65221c3b916c2d31d03d9f5a96fcb6022
Viktor Belous <vyntyk@yandex.ru>
2022-08-18 22:58:05 +0300

    Задача 36

---
+++ Papka2/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный
+ случайными числами. Найдите сумму элементов, 
+ стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите элементы массива через запятую: ");
+int[] array = getArray(ReadLine());
+Write($"[{String.Join(", ", array)}]");
+WriteLine($"Сумма элементов -> {sumOfElements(array, array.Length)}");
+
+int[] getArray(string arrStr) {
+
+    string[] arr = arrStr.Split(new char[]{' ', ','}, StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[arr.Length];
+    for(int i=0; i < arr.Length; i++) 
+    {
+        result[i] = int.Parse(arr[i]); 
+    }
+    return result;
+}
+
+int sumOfElements(int[] array, int arraySize)
+{
+    int sum = 0;
+
+    for(int i=0; i < arraySize; i++)
+    {
+        if (i % 2 != 0) {
+            sum = array[i] + sum;
+        }
+    }
+    return sum;
+}
